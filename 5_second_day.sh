537a3f21e19b0f3b68bc7b85d08a2359077eab6a
SadovnichiyDmitry <34300867+SadovnichiyDmitry@users.noreply.github.com>
2019-12-18 17:04:36 +0300

    Update 5_second_day.sh

--- 5_second_day.sh
+++ 5_second_day.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-_etc_files=$(ls -la /etc/ | grep "^-" | wc -l)
-_username=$(whoami)
-if [ "$#" != 1 ]; then
-echo "you entered wrong number of arguments, please enter one argument."
-else
-  if [ -d /tmp/$1 ]; then
-  echo "directory exists"
-  else
-  echo "directory not exist, i will create this one"
-  mkdir /tmp/$1
-  mkdir /tmp/$_username
-  for ((n=1; n <= $_etc_files; n++))
-    do
-    cp /dev/null /tmp/$1/$n
-    ln -s /tmp/$1/$n /tmp/$_username/sl$n
-  done
-  ls -l /tmp/$1 >> filelist.txt
-  ls -l /tmp/$_username >> filelist.txt
-fi
-fi
 
+etc_files=$(ls -la /etc/ | grep "^-" | wc -l)
+username=$(whoami)
+
+if [ "$#" != 1 ]; then
+  echo "please enter one argument."
+else
+  mkdir -p /tmp/$1/$username
+  for ((n=1; n <= $etc_files; n++)); do
+    touch /tmp/$1/$n
+    ln -s /tmp/$1/$n /tmp/$1/$username/sl$n
+  done
+  ls -lR /tmp/$1 >> filelist.txt
+fi
